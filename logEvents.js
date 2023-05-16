3821de8bc89b1de35ffec7d34d96a842a5c14f3c
Min Khaung <minkhaung.mkks@gmail.com>
2023-05-16 19:56:11 +0630

    created an event logger

---
+++ logEvents.js
@@ -0,0 +1,22 @@
+const {format} = require('data-fns')
+const {v4 : uuid} = require('uuid')
+
+const fs = require('fs')
+const fsPromise = require('fs').promises;
+const path = require('path')
+
+const logEvents = async (msg) =>{
+    const timestamp = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+    const logItem = `${timestamp}\t${uuid()}\t${msg}\n`;
+    console.log(logItem);
+    try {
+        if(!fs.existsSync(path.join(__dirname,'logs'))){
+            await fsPromise.mkdir(path.join(__dirname,'logs'))
+        }
+        await fsPromise.appendFile(path.join(__dirname,'logs','server_logs.txt'))
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+module.exports=logEvents;
