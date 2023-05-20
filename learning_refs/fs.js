f0331a98f2430a5bddbd6aa7a80c1ec7b049e95c
Min Khaung <minkhaung.mkks@gmail.com>
2023-05-20 22:45:06 +0630

    Fs Promise

--- learning_refs/fs.js
+++ learning_refs/fs.js
@@ -21,6 +21,36 @@ fs.readFile(path.join(__dirname,'fs_files','test.txt'), 'utf-8', (err,data)=>{
 })
 
 
+fs.writeFile(path.join(__dirname,'fs_files','reply.txt'),'Hello From World',(err)=>{
+    if (err) throw err
+    console.log('Write Complete')
+})
+
+// Append will create a file if the file doesnt already exists but will not do this for the folder.
+fs.appendFile(path.join(__dirname,'fs_files','append.txt'),'Hello From Append',(err)=>{
+    if (err) throw err
+    console.log('Append Complete')
+})
+
+fs.rename(path.join(__dirname,'fs_files','append.txt'),path.join(__dirname,'fs_files','rename.txt'),(err)=>{
+    if (err) throw err
+    console.log('Rename Complete')
+})
+
+
+// Use fsPromise to avoid callback hell
+
+const fsProTest = async ()=>{
+    let data = await fsPromise.readFile(path.join(__dirname,'fs_files','test.txt'),'utf-8');
+    await fsPromise.writeFile(path.join(__dirname,'fs_files','promise.txt'),data)
+    await fsPromise.appendFile(path.join(__dirname,'fs_files','promise.txt'),`\n\nWorld:.....`)
+    await fsPromise.rename(path.join(__dirname,'fs_files','promise.txt'),path.join(__dirname,'fs_files','promiseDone.txt'))
+     data = await fsPromise.readFile(path.join(__dirname,'fs_files','promiseDone.txt'),'utf-8');
+    console.log(data)
+}
+
+fsProTest()
+
 // Runs on all errors that wasnt caught inside a try catch.
 process.on('uncaughtException', err=>{
     console.error('There was an uncaught error : ' + err )
