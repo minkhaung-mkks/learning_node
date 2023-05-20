105fe94d8556b587f2d0f4250f1038d385fc543b
Min Khaung <minkhaung.mkks@gmail.com>
2023-05-20 22:30:53 +0630

    FS read basics

--- learning_refs/fs.js
+++ learning_refs/fs.js
@@ -1,3 +1,27 @@
 const fs = require('fs');
 
 const fsPromise = require('fs').promises;
+
+// Interesting Fact: These Console Logs are random in their order of execution, buffered data something showing up 2nd.
+
+fs.readFile('./fs_files/test.txt', (err, data)=>{
+    if(err) throw err
+    //Prints buffered Data <Buffer 48 54 34...>
+    console.log(data) 
+    //Prints the actual text
+    console.log(data.toString())
+})
+
+
+// By Placing the encoding inside the call, we can get the actual data.
+fs.readFile('./fs_files/test.txt', 'utf-8', (err,data)=>{
+    if(err) throw err;
+    console.log(data)
+})
+
+
+// Runs on all errors that wasnt caught inside a try catch.
+process.on('uncaughtException', err=>{
+    console.error('There was an uncaught error : ' + err )
+    process.exit(1);
+})
