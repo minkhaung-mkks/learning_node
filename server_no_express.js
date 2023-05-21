565000b9526e687c9327878a3160481619413e6c
Min Khaung <minkhaung.mkks@gmail.com>
2023-05-21 23:12:06 +0630

    Another way to handle url paths

--- server_no_express.js
+++ server_no_express.js
@@ -9,14 +9,30 @@ const PORT = process.env.PORT || 3500;
 const server = http.createServer((req,res)=>{
     console.log(req.url, req.method)
     let fileLocale;
-    if(req.url === '/' || req.url === 'index.html'){
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        fileLocale = path.join(__dirname,'Web_pages','index.html');
-        fs.readFile(fileLocale, 'utf-8', (err,data)=>{
-            res.end(data)
-        })
+    // Good Way
+    // if(req.url === '/' || req.url === 'index.html'){
+    //     res.statusCode = 200;
+    //     res.setHeader('Content-Type', 'text/html');
+    //     fileLocale = path.join(__dirname,'Web_pages','index.html');
+    //     fs.readFile(fileLocale, 'utf-8', (err,data)=>{
+    //         res.end(data)
+    //     })
+    // }
+    // Another way
+    switch(req.url){
+        case'/':
+            res.statusCode=200;
+            fileLocale=path.join(__dirname,'Web_pages', 'index.html');
+            fs.readFile(fileLocale, 'utf-8', (err,data)=>{
+                res.end(data)
+            })
+        break;
+        default:
+            // Give 404 Page
+            break;
     }
 });
+
+
 
 server.listen(PORT, ()=> console.log('Server Running on Port : ' + PORT))
