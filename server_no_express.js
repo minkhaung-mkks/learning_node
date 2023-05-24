1f6b13127a7051a57930b028aba1a53eddaa658d
Min Khaung <minkhaung.mkks@gmail.com>
2023-05-24 21:25:13 +0630

    Fixed other file types not displaying or getting served properly

--- server_no_express.js
+++ server_no_express.js
@@ -3,12 +3,21 @@ const http = require('http')
 const fs = require('fs')
 const fsPromise = require('fs').promises
 const path = require('path')
-const {v4: uuid} = require('uuid')
+const logEvents = require('./logEvents');
+const { v4: uuid } = require('uuid')
+const EventEmitter = require('events');
+
+class Emitter extends EventEmitter { };
+
+const myEmitter = new Emitter();
+
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
 
 const PORT = process.env.PORT || 3500;
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
+    myEmitter.emit('log', `${req.method}: ${req.url}`, 'Req_logs.log')
     let fileLocale;
     let fileExtentsion;
     let contentType;
@@ -41,81 +50,87 @@ const server = http.createServer((req,res)=>{
     // }
 
     // Best Way
-    switch(fileExtentsion){
+    switch (fileExtentsion) {
         case '.css':
-            contentType="text/css";
+            contentType = "text/css";
             break;
         case '.js':
-            contentType="text/javascript";
+            contentType = "text/javascript";
             break;
         case '.json':
-            contentType="application/json";
+            contentType = "application/json";
             break;
         case '.jpg':
-            contentType="image/jpg";
+            contentType = "image/jpg";
             break;
         case '.png':
-            contentType="text/png";
+            contentType = "text/png";
             break;
         case '.txt':
-            contentType="text/plain";
+            contentType = "text/plain";
             break;
         default:
-            contentType="text/html";
+            contentType = "text/html";
     }
 
     // if req.url is / or ends with / give the index page
     // else if the contentType is text/html give the requested page
     // if the contentType is not text/html give the requested resource, wether it be image or json.
-    fileLocale = contentType === 'text/html' && req.url === '/' 
-    ? path.join(__dirname,'Web_pages','index.html')
-    : contentType === 'text/html' && req.url.slice(-1) === '/'
-    ? path.join(__dirname,'Web_pages','index.html')
-    : contentType === 'text/html' 
-    ? path.join(__dirname,'Web_pages',req.url)
-    : path.join(__dirname, req.url)
+    fileLocale = contentType === 'text/html' && req.url === '/'
+        ? path.join(__dirname, 'Web_pages', 'index.html')
+        : contentType === 'text/html' && req.url.slice(-1) === '/'
+            ? path.join(__dirname, 'Web_pages', 'index.html')
+            : contentType === 'text/html'
+                ? path.join(__dirname, 'Web_pages', req.url)
+                : path.join(__dirname, req.url)
 
     // if the user forgets to put the extention behind the url, default to .html
-    // if(!fileExtentsion && req.url.slice(-1) !== '/') fileLocale +='.html'
+    if (!fileExtentsion && req.url.slice(-1) !== '/') fileLocale += '.html'
 
     const fileExists = fs.existsSync(fileLocale);
 
-    const servreFile = async (filePath, contentType, res)=>{
+    const servreFile = async (filePath, contentType, res) => {
         try {
-            const data = await fsPromise.readFile(filePath, 'utf-8');
-            res.writeHead(200, {'Content-Type': contentType})
-            res.end(data);
+            const rawData = await fsPromise.readFile(filePath, !contentType.includes('image') ? 'utf-8' : '');
+            const data = contentType === 'application/json' ? JSON.parse(rawData) : rawData
+            res.writeHead(
+                filePath.includes('404') ? 404 : 200, { 'Content-Type': contentType })
+
+            res.end(
+                contentType === 'application/json' ? JSON.stringify(data) : data
+            );
         } catch (error) {
             console.error(error);
-            res.statusCode =500;
+            myEmitter.emit('log', `${error.name}: ${error.message}`, 'Server_error.log')
+            res.statusCode = 500;
             res.end();
         }
     }
-    if(fileExists){
+    if (fileExists) {
         // serve the file
         console.log('Serving file')
-        servreFile(fileLocale,contentType,res)
+        servreFile(fileLocale, contentType, res)
     }
-    else{
+    else {
         //404
         //redirect (301)
         console.log('Redirecting File')
         let parsedPath = path.parse(fileLocale)
         console.log(parsedPath);
         // .base Because thats where the file is located on the parsedPath object.
-        switch(parsedPath.base){
+        switch (parsedPath.base) {
             // Redirect
             case 'testPage.html':
-                res.writeHead(301, {'Location':'/new_page.html'})
+                res.writeHead(301, { 'Location': '/new_page.html' })
                 res.end();
                 break;
             default:
                 // 404
-                servreFile(path.join(__dirname, 'Web_pages','404.html'), 'text/html',res)
+                servreFile(path.join(__dirname, 'Web_pages', '404.html'), 'text/html', res)
         }
     }
 });
 
 
 
-server.listen(PORT, ()=> console.log('Server Running on Port : ' + PORT))
+server.listen(PORT, () => console.log('Server Running on Port : ' + PORT))
