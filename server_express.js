80984b12f0ca29a1227b81ab56ed61f9d439596b
Min Khaung <minkhaung.mkks@gmail.com>
2023-05-27 22:39:25 +0630

    basic app routers

--- server_express.js
+++ server_express.js
@@ -4,4 +4,16 @@ const path = require('path')
 const PORT = process.env.PORT || 3500;
 const app = express();
 
+app.get('^/$|/index(.html)?', (req,res)=>{
+    res.sendFile(path.join(__dirname,'Web_pages','index.html'));
+})
+
+app.get('/new_page(.html)?',(req,res)=>{
+    res.sendFile(path.join(__dirname,'Web_pages','new_page.html'))
+})
+
+app.get('/old_page(.html)?',(req,res)=>{
+    res.redirect(301, '/new_page')
+})
+
 app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`))
