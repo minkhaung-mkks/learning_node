f6fdcd8b95c1da088dcb850fa2af878c66754dad
Min Khaung <minkhaung.mkks@gmail.com>
2023-09-08 23:10:24 +0630

    Fixed Name typo bugs

--- routes/root.js
+++ routes/root.js
@@ -1,45 +1,45 @@
 const express = require('express');
-const router =express.router();
+const router = express.Router();
 const path = require('path');
 
 //These Gets are called Route Handlers
-router.get('^/$|/index(.html)?', (req,res)=>{
-    res.sendFile(path.join(__dirname,'Web_pages','index.html'));
+router.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'Web_pages', 'index.html'));
 })
 
-router.get('/new_page(.html)?',(req,res)=>{
-    res.sendFile(path.join(__dirname,'Web_pages','new_page.html'))
+router.get('/new_page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'Web_pages', 'new_page.html'))
 })
 
-router.get('/old_page(.html)?',(req,res)=>{
+router.get('/old_page(.html)?', (req, res) => {
     res.redirect(301, '/new_page')
 })
 
 // Route Chaining
-router.get('/chain(.html)?',(req,res,next)=>{
+router.get('/chain(.html)?', (req, res, next) => {
     console.log('One')
     next()//
 },
-(req,res)=>{
-    res.send("Chained")
-}
+    (req, res) => {
+        res.send("Chained")
+    }
 )
 
 // Typical way of doing chaining
-const one = (req,res,next)=>{
+const one = (req, res, next) => {
     console.log('one')
     next()
 }
 
-const two = (req,res,next)=>{
+const two = (req, res, next) => {
     console.log('two')
     next()
 }
 
-const three = (req,res)=>{
+const three = (req, res) => {
     res.send("Chained")
 }
 
-router.get('/tchained(.html)?',[one,two,three])
+router.get('/tchained(.html)?', [one, two, three])
 
 module.exports = router;
--- routes/subdir.js
+++ routes/subdir.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const router =express.router();
+const router = express.Router();
 const path = require('path');
 
-router.get('^/$|/index(.html)?', (req,res)=>{
-    res.sendFile(path.join(__dirname,'..','Web_pages','subdirs','index.html'));
+router.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'Web_pages', 'subdirs', 'index.html'));
 })
-router.get('/test(.html)?', (req,res)=>{
-    res.sendFile(path.join(__dirname,'..','Web_pages','subdirs','test.html'));
+router.get('/test(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'Web_pages', 'subdirs', 'test.html'));
 })
 
-module.exports=router;
+module.exports = router;
