01a2928755068b27fb96c3a6182ee4a2befc73e5
Min Khaung <minkhaung.mkks@gmail.com>
2023-09-12 19:10:42 +0630

    Basic REST API

---
+++ routes/apis/employees.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+const data = {}
+data.employees = require('../../data/employees.json')
+
+router.route('/')
+    .get((req, res) => {
+        res.json(data.employees)
+    })
+    .post((req, res) => {
+        const newEmployee = req.body.employee
+        data.employees.push(newEmployee)
+        // Append to JSON file
+    })
+    .put((req, res) => {
+        //Update Employee
+        const newEmployees = req.body.employees
+        //Update the json file with the new employee list
+    })
+    .delete((req, res) => {
+        res.json({
+            "id": req.body.id
+        })
+    })
