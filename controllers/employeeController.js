4a2c19b933dea513e093c4ec95a3069cadce2e66
Min Khaung <minkhaung.mkks@gmail.com>
2023-09-13 19:56:04 +0630

    Bug Fixes and Change to fs.writefileSync due to some delays causing crash

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -15,29 +15,33 @@ const getAllEmployee = (req, res) => {
     res.json(data.employees)
 }
 
-const addNewEmployee = (req, res) => {
+const addNewEmployee = async (req, res) => {
     const newEmployee = {
-        "id": data.employees.length + 1,
+        "id": data.employees[data.employees.length - 1].id + 1 || 1,
         "firstName": req.body.firstName,
         "lastName": req.body.lastName,
     }
+    await updateFetchedData()
     data.employees.push(newEmployee)
     // Append to JSON file
     updateData(data.employees, res)
 }
 
-const updateEmployees = (req, res) => {
+const updateEmployees = async (req, res) => {
     //Update Employee
     const reqId = parseInt(req.body.id)
+    await updateFetchedData()
     const employee = data.employees.filter((emp) => (emp.id === reqId))
     if (!employee) {
         return res.status(400).json({ "message": `No such employee with id of ${id} found.` })
     }
-    if (req.body.firstName) employee.firstName = req.body.firstName
-    if (req.body.lastName) employee.lastName = req.body.lastName
+    console.log(employee)
+    if (req.body.firstName) employee[0].firstName = req.body.firstName
+    if (req.body.lastName) employee[0].lastName = req.body.lastName
     const otherEmployees = data.employees.filter(emp => (
         emp.id !== reqId
     ))
+    console.log(employee)
     const newUnsortedArray = [...otherEmployees, ...employee]
     const sortedArray = newUnsortedArray.sort((a, b) => a.id > b.id ? 1 : a.id < b.id ? -1 : 0)
     // console.log(sortedArray)
@@ -45,8 +49,9 @@ const updateEmployees = (req, res) => {
     updateData(sortedArray, res)
 }
 
-const deleteEmployees = (req, res) => {
+const deleteEmployees = async (req, res) => {
     const reqId = parseInt(req.body.id)
+    await updateFetchedData()
     const otherEmployees = data.employees.filter(emp => (
         emp.id !== reqId
     ))
@@ -60,17 +65,18 @@ const getEmployee = (req, res) => {
 }
 
 const updateJsonFile = async (data, res) => {
-    fs.writeFile(filePath, JSON.stringify(data), (err) => {
-        if (err) {
-            res.send("Error writing to file");
-        } else {
-            res.send("Data updated successfully " + JSON.stringify(data));
-        }
-    });
+
+    try {
+        fs.writeFileSync(filePath, JSON.stringify(data));   //'a+' is append mode
+        res.send("Data updated successfully " + JSON.stringify(data));
+    } catch (err) {
+        console.error(err);
+        res.send("Error writing to file");
+    }
 }
 
 const updateData = async (data, res) => {
-    updateJsonFile(data, res)
+    await updateJsonFile(data, res)
     updateFetchedData()
 }
 
