0b65411811374d2f0569b3029847c52ab9bb67f8
Min Khaung <minkhaung.mkks@gmail.com>
2023-09-15 23:58:31 +0630

    Example of limiting auth to a few routes

--- routes/apis/employees.js
+++ routes/apis/employees.js
@@ -2,13 +2,15 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 const employeeController = require('../../controllers/EmployeeController')
-
+// const verifyJWT = require('../../middleware/VerifyJWT')
 
 router.route('/')
     .get(employeeController.getAllEmployee)
     .post(employeeController.addNewEmployee)
     .put(employeeController.updateEmployees)
     .delete(employeeController.deleteEmployees)
+// IF you want to gate keep only a few routes
+//  .get(verifyJWT,employeeController.getAllEmployee)
 
 router.route('/:id')
     .get(employeeController.getEmployee)
