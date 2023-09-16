655531a4168ed31050ec4df8c4932f1c46774487
Min Khaung <minkhaung.mkks@gmail.com>
2023-09-16 22:29:14 +0630

    Using the verify Role middleware to lock some paths

--- routes/apis/employees.js
+++ routes/apis/employees.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const ROLES_LIST = require('../../configs/roles')
 const employeeController = require('../../controllers/EmployeeController')
+const verifyRoles = require('../../middleware/verifyRoles')
+
 // const verifyJWT = require('../../middleware/VerifyJWT')
 
 router.route('/')
     .get(employeeController.getAllEmployee)
-    .post(employeeController.addNewEmployee)
-    .put(employeeController.updateEmployees)
-    .delete(employeeController.deleteEmployees)
+    .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Edtior), employeeController.addNewEmployee)
+    .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Edtior), employeeController.updateEmployees)
+    .delete(verifyRoles(ROLES_LIST.Admin), employeeController.deleteEmployees)
 // IF you want to gate keep only a few routes
 //  .get(verifyJWT,employeeController.getAllEmployee)
 
