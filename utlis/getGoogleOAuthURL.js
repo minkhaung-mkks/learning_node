b2f7c89f20dcba7ce62f869fac6835ee74a58207
Min Khaung <minkhaung.mkks@gmail.com>
2023-09-28 23:37:19 +0630

    Construct the OAuth URI

---
+++ utlis/getGoogleOAuthURL.js
@@ -0,0 +1,22 @@
+
+
+const getGoogleOAuthURL = () => {
+    const oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+
+    const params = {
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        redirect_uri: process.env.GOOGLE_REDIRECT_URL,
+        response_type: "code",
+        prompt: "consent",
+        access_type: "offline",
+        scope: [
+            "https://www.googleapis.com/auth/userinfo.email",
+            "https://www.googleapis.com/auth/userinfo.profile"
+        ].join(" ")
+    }
+    const qs = new URLSearchParams(params)
+
+    return `${oauth2Endpoint}?${qs.toString()}`
+}
+
+export default getGoogleOAuthURL
